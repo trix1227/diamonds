06e748d4cbd57210966740051c4c4bbcb933f8c3
Trixie <73319495+trix1227@users.noreply.github.com>
2021-04-10 17:30:13 +0800

    Add files via upload

--- diamonds.R
+++ diamonds.R
@@ -8,6 +8,11 @@
 ggplot(data=diamonds,aes(price))+geom_histogram()
 #Average of the diamonds price is around $3933, with min $326 (ideal& premium cut) and max $18823 (premium cut)
 
+#and lets see the distribution of each cut
+ggplot(data=Diamonds,aes(cut))+geom_histogram()
+
+#from the data collected, most of our data is from ideal, premium, very good, good & fair
+
 #Next, let's see different average price for diamonds by color
 #to check how does the average price looks like if we sort by color? 
 ggplot(data=diamonds,aes(color,price))+geom_boxplot(aes(color=color))
@@ -27,3 +32,13 @@ ggplot(data=Diamonds,aes(cut,avg_carat))+geom_boxplot(aes(color=cut))
 
 #by_cut price for ideal is still lower than the rest. so let's plot a point graph to see by cut & color
 ggplot(data=Diamonds,aes(carat,avg_carat))+geom_point(aes(color=color))+facet_wrap(cut~color)
+
+#Let's deep dive into ideal data set
+Diamonds%>%ggplot(aes(carat))+geom_histogram()+facet_wrap(.~cut)
+#majority of ideal cut is from small carat,
+#let's do the same plot to see if majority of ideal cut prices falls below $5K
+Diamonds%>%ggplot(aes(price))+geom_histogram()+facet_wrap(.~cut)
+
+#from the plot, it confirms - thus this could explain as the ideal diamonds are mostly from small carat
+#thus the avg_price for this ideal cut is lower than the rest of the other cuts
+
